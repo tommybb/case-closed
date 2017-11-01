c1c767faabf49040d93301c53e744e9397477ddb
Stevo <blazejek@gmail.com>
2017-11-01 14:41:37 +0100

    Refactoring step #3/2 - implementation/algorithm unification -> services implementation

--- app/services/import_gcpd_cases.rb
+++ app/services/import_gcpd_cases.rb
@@ -1,32 +1,20 @@
 class ImportGcpdCases
   def self.call
     GcpdWrapper.cases.each do |case_details|
-      # Do not duplicate cases
-      new_case = Case.where(case_uid: case_details.uid).first_or_initialize
-      next if new_case.persisted?
-
-      # Save case
-      new_case.name = case_details.name
-      new_case.department_code = 'gcpd'
-      new_case.officer_email = case_details.officer_email
-      new_case.officer_name = case_details.officer_name
-      new_case.description = case_details.description
-      new_case.important = case_details.important
-      new_case.save
-
-
-      # Assign hero to case
-      hero_with_least_cases_id = Hero.
-        all.
-        map{|hero| [hero.assigned_cases.count, hero.id] }.
-        sort_by(&:first).
-        first.
-        last
-      Hero.find(hero_with_least_cases_id).assigned_cases << new_case
-
-      # Notify officer that his case will be solved soon (unless spiderman was assigned
-      # - hahaha, just kidding)
-      NotificationsMailer.case_assignment_notification(new_case).deliver_later
+      unless Case.where(case_uid: case_details.uid).exists?
+        case_details = Case.create(
+          name: case_details.name,
+          case_uid: case_details.uid,
+          department_code: 'gcpd',
+          officer_email: case_details.officer_email,
+          officer_name: case_details.officer_name,
+          description: case_details.description,
+          important: case_details.important
+        )
+        _, id = Hero.all.map{|hero| [hero.assigned_cases.count, hero.id] }.sort_by(&:first).first
+        HeroCase.create!(hero_id: id, case_id: case_details.id)
+        NotificationsMailer.case_assignment_notification(case_details).deliver_later
+      end
     end
   end
 end
--- app/services/import_nypd_cases.rb
+++ app/services/import_nypd_cases.rb
@@ -1,23 +1,19 @@
 class ImportNypdCases
   def self.call
-    new.call
-  end
-
-  def call
-    NypdWrapper.cases.each do |kejs|
-      unless Case.where(case_uid: kejs.uid).exists?
-        kejs = Case.create(
-          name: kejs.name,
-          case_uid: kejs.uid,
+    NypdWrapper.cases.each do |case_details|
+      unless Case.where(case_uid: case_details.uid).exists?
+        case_details = Case.create(
+          name: case_details.name,
+          case_uid: case_details.uid,
           department_code: 'nypd',
-          officer_email: kejs.officer_email,
-          officer_name: kejs.officer_name,
-          description: kejs.description,
-          important: kejs.important
+          officer_email: case_details.officer_email,
+          officer_name: case_details.officer_name,
+          description: case_details.description,
+          important: case_details.important
         )
         _, id = Hero.all.map{|hero| [hero.assigned_cases.count, hero.id] }.sort_by(&:first).first
-        HeroCase.create!(hero_id: id, case_id: kejs.id)
-        NotificationsMailer.case_assignment_notification(kejs).deliver_later
+        HeroCase.create!(hero_id: id, case_id: case_details.id)
+        NotificationsMailer.case_assignment_notification(case_details).deliver_later
       end
     end
   end
